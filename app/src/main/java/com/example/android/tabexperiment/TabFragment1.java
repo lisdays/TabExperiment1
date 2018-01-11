58b3c5e0c8aae432dd2655116708c0b2a81759bb
lisdays <lisdayuni02@gmail.com>
2018-01-11 22:57:31 +0700

    Initial commit

---
+++ app/src/main/java/com/example/android/tabexperiment/TabFragment1.java
@@ -0,0 +1,28 @@
+package com.example.android.tabexperiment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TabFragment1 extends Fragment {
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+            return inflater.inflate(R.layout.tab_fragment1, container, false);
+        }
+}
+
+
+
+
